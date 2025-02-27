cb9c8a507ecef2128d41e670cd0cc27f84a09f4e
Joppe Hoekstra <joppehoekstra@icloud.com>
2025-02-27 09:00:03 +0100

    Performance improvements

--- components/Content/Image.vue
+++ components/Content/Image.vue
@@ -3,7 +3,7 @@
     <figure>
       <div class="rounded-md overflow-hidden">
         <NuxtPicture format="avif,webp" :src="props.imageURL" :alt="props.alt" width="2000px" height="1000px"
-          class="w-full " />
+          class="w-full " loading="lazy" />
       </div>
 
       <figcaption class="text-neutral-600 italic text-sm text-right" v-if="props.caption">{{ props.caption }}
