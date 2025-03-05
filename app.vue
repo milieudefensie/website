d8b0d45f7606d8decfce89ab93752f08831c4da8
Joppe Hoekstra <joppehoekstra@icloud.com>
2025-03-05 09:50:06 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -6,6 +6,15 @@
 <script setup>
 
 useHead({
+  link: [
+    {
+      rel: 'preload',
+      type: 'font/woff2',
+      href: '/Ringold-Soft.woff2',
+      as: 'font',
+      crossorigin: ''
+    },
+  ],
   script: [
     {
       id: 'hs-script-loader',
