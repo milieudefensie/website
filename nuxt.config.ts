83b8375b5309784fb83187e7561eb2af8cb228d8
Joppe Hoekstra <joppehoekstra@icloud.com>
2025-03-13 16:33:26 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/image',
     ['unplugin-icons/nuxt', { scale: 1.5 }],
-    '@nuxt/scripts',
+    // '@nuxt/scripts', FIX NEEDED: [error] [ nuxi ] Nuxt Build Error: [vite]: Rollup failed to resolve import "unenv/runtime/mock/noop" from "/opt/buildhome/repo/node_modules/.pnpm/@nuxt+scripts@0.10.5_@types+youtube@0.1.0_@unhead+vue@1.11.20_typescript@5.8.2/node_modules/@nuxt/scripts/dist/runtime/validation/mock.js"
     '@pinia/nuxt',
   ],
   devtools: { enabled: true },
